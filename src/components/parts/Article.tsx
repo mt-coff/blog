2744c4630e4cc573f6c396fc77cc04da135fbe10
mt-coff <mt.coff.00@gmail.com>
2020-01-10 19:00:31 +0900

    h要素のマージン調整

--- src/components/parts/Article.tsx
+++ src/components/parts/Article.tsx
@@ -3,22 +3,22 @@ import React from "react";
 
 const Article: Components = {
   h1: (props: JSX.IntrinsicElements["h1"]) => (
-    <h1 className="text-3xl border-b font-semibold m-2" {...props} />
+    <h1 className="text-3xl border-b font-semibold my-2" {...props} />
   ),
   h2: (props: JSX.IntrinsicElements["h2"]) => (
-    <h2 className="text-2xl border-b font-semibold m-2" {...props} />
+    <h2 className="text-2xl border-b font-semibold my-2" {...props} />
   ),
   h3: (props: JSX.IntrinsicElements["h3"]) => (
-    <h3 className="text-xl border-b font-semibold m-2" {...props} />
+    <h3 className="text-xl border-b font-semibold my-2" {...props} />
   ),
   h4: (props: JSX.IntrinsicElements["h4"]) => (
-    <h4 className="text-lg border-b font-semibold m-2" {...props} />
+    <h4 className="text-lg border-b font-semibold my-2" {...props} />
   ),
   h5: (props: JSX.IntrinsicElements["h5"]) => (
-    <h5 className="text-base border-b font-semibold m-2" {...props} />
+    <h5 className="text-base border-b font-semibold my-2" {...props} />
   ),
   h6: (props: JSX.IntrinsicElements["h6"]) => (
-    <h6 className="text-sm border-b font-semibold m-2" {...props} />
+    <h6 className="text-sm border-b font-semibold my-2" {...props} />
   ),
   p: (props: JSX.IntrinsicElements["p"]) => (
     <p className="py-2 leading-relax" {...props} />
