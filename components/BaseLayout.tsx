3dab222b42ac208996020badbd519f3f95d0eee1
mt-coff <mt.coff.00@gmail.com>
2020-12-18 02:16:07 +0900

    全体のレイアウトのベース

---
+++ components/BaseLayout.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+type Props = {
+  className?: string
+}
+
+const BaseLayout: FC<Props> = ({ children, className }) => {
+  return (
+    <>
+      <Header />
+      <main className={`flex flex-1 ${className}`}>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default BaseLayout;
