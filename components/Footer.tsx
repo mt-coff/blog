6c4b8ce767030494e59b27cb7efa6d4728d29ab0
mt-coff <mt.coff.00@gmail.com>
2020-12-18 02:15:49 +0900

    フッター

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import { VFC } from "react";
+
+const Footer: VFC = () => {
+  return (
+    <footer className="flex flex-row justify-center py-4 sm:mx-32 border-t">
+      <small>©2020 mt_coff All Rights Reserved.</small>
+    </footer>
+  );
+};
+
+export default Footer;
