8cb485d0f5026b5c831a44a4fe9c98fdd7f9cf0f
mt-coff <mt.coff.00@gmail.com>
2020-12-18 01:19:26 +0900

    404ページ

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import { VFC } from "react";
+import Image from "next/image";
+import BaseLayout from "../components/BaseLayout";
+
+const NotFound: VFC = () => {
+  return (
+    <BaseLayout>
+      <div className="flex items-center justify-center flex-1">
+        <Image src="/img/mt_coffee.png" width={128} height={128} layout="fixed" />
+        <span className="text-3xl pl-8 ml-8 border-l-2 h-24 flex items-center">
+          404 Not Found
+        </span>
+      </div>
+    </BaseLayout>
+  );
+};
+
+export default NotFound;
