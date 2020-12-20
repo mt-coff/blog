bd20829674c09d8c78c6d5851bf5adbea9ea002e
mt-coff <mt.coff.00@gmail.com>
2020-12-21 08:53:26 +0900

    記事タイトルコンポーネント

---
+++ components/PostTitle/index.tsx
@@ -0,0 +1,18 @@
+import { VFC } from "react";
+import { FrontMatter } from "../../types/frontMatter";
+import style from "./index.module.css"
+
+type Props = {
+  frontMatter: FrontMatter;
+};
+
+const PostTitle: VFC<Props> = ({ frontMatter }) => {
+  return (
+    <section className={style["post-title"]}>
+      <h1 className="text-5xl font-semibold text-center">{frontMatter.title}</h1>
+      <time className="w-full text-right block px-2 pt-2">{frontMatter.created}</time>
+    </section>
+  );
+};
+
+export default PostTitle;
