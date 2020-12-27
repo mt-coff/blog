4ad8dd4042459d2ae24f292cc3befe685a955105
mt-coff <mt.coff.00@gmail.com>
2020-12-27 11:33:43 +0900

    タイトルにリンク

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 import { VFC } from "react";
+import Link from "next/link";
 
 const Header: VFC = () => {
   return (
     <header className="h-16 bg-mt-green flex justify-center sm:justify-start items-center px-6">
-      <h1 className="text-white text-4xl font-bold">mt_coff's blog</h1>
+      <h1 className="text-white text-4xl font-bold">
+        <Link href="/">
+          mt_coff's blog
+        </Link>
+      </h1>
     </header>
   );
 };
