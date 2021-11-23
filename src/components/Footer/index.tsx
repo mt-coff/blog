32f210196f7f3f5ea8efac0295448572fae44c39
mt-coff <mt.coff.00@gmail.com>
2021-11-24 00:44:57 +0900

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,33 @@
-import { Box } from "@chakra-ui/layout";
+import { Flex, Text, HStack, Link, Icon } from "@chakra-ui/react";
+import { FaGithubSquare, FaTwitterSquare } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <Box as="footer" borderTop="solid 1px" borderColor="gray.200" mx="8" py="4">
-      faa
-    </Box>
+    <Flex
+      as="footer"
+      borderTop="solid 1px"
+      borderColor="gray.200"
+      mx="8"
+      py="4"
+      direction="column"
+      align="center"
+    >
+      <HStack>
+        <Link
+          target="_blank"
+          href="https://github.com/mt-coff"
+          rel="noopener noreferrer"
+        >
+          <Icon as={FaGithubSquare} h={6} w={6} />
+        </Link>
+        <Link
+          target="_blank"
+          href="https://twitter.com/mt_coff"
+          rel="noopener noreferrer"
+        >
+          <Icon as={FaTwitterSquare} h={6} w={6} />
+        </Link>
+      </HStack>
+    </Flex>
   );
 };
