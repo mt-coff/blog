69e11698a02fcbdacf6fe431c2dba07cbfabe3a2
mt-coff <mt.coff.00@gmail.com>
2021-11-24 00:46:52 +0900

    impl top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
-import type { NextPage } from "next";
+import { getPosts } from "@/api/getPosts";
+import type { GetStaticProps, NextPage } from "next";
+import React from "react";
+import { PostList } from "../components/PostList";
 
-const Home: NextPage = () => {
-  return <div>a</div>;
+type Props = {
+  posts: Post[];
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
+  return (
+    <>
+      <PostList posts={posts}></PostList>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props, {}> = async () => {
+  const posts = await getPosts({ limit: 6 });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Home;
