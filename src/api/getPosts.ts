b38ed1c7cdd295616b00ff5ef8578b5e6cb4940b
mt-coff <mt.coff.00@gmail.com>
2021-11-23 23:38:10 +0900

    add fetch post

---
+++ src/api/getPosts.ts
@@ -0,0 +1,24 @@
+import { microCmsApi } from "@/utils/microCmsApi";
+
+type PostResponse = {
+  contents: Post[];
+};
+
+export const getPosts = async ({
+  limit = 100,
+  filters,
+}: {
+  limit?: number;
+  filters?: string;
+}) => {
+  const params = new URLSearchParams({ limit: limit.toString() });
+  if (filters) {
+    params.append("filters", filters);
+  }
+  const { contents } = await microCmsApi<PostResponse>(
+    `/posts?${params.toString()}`,
+    "GET"
+  );
+
+  return contents;
+};
