f462ff243d82260b359c4a319c1808c92a22ae56
mt-coff <mt.coff.00@gmail.com>
2021-11-23 23:37:54 +0900

    Delte header underline

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex, Heading, Box } from "@chakra-ui/react";
+import { Flex, Heading, Box, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { ModeToggleButton } from "../ModeToggleButton";
 
 export const Header = () => {
   return (
     <Flex as="header" shadow="sm" justify="space-between" p="4">
-      <Heading as="h1">blog.mt_coff</Heading>
+      <Heading as="h1">
+        <NextLink href={"/"}>
+          <Link _hover={{ textDecor: "none" }}>blog.mt_coff</Link>
+        </NextLink>
+      </Heading>
       <Box>
         <ModeToggleButton />
       </Box>
