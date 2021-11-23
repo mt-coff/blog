a4717158f7d1802508ce0bbdd67381e971475540
mt-coff <mt.coff.00@gmail.com>
2021-11-24 00:46:41 +0900

    adjust post

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,16 +1,22 @@
-import { List } from "@chakra-ui/react";
+import { List, Text, Icon } from "@chakra-ui/react";
 import { PostListItem } from "../PostListItem";
+import { MdInfo } from "react-icons/md";
 
 type Props = {
   posts: Post[];
 };
 
 export const PostList = ({ posts }: Props) => {
-  return (
+  return posts && posts.length > 0 ? (
     <List>
       {posts.map((post) => (
         <PostListItem post={post} key={post.id} />
       ))}
     </List>
+  ) : (
+    <Text display="flex" alignItems="center">
+      <Icon as={MdInfo} mr={1} />
+      記事が見つかりませんでした
+    </Text>
   );
 };
--- src/components/PostListItem/index.tsx
+++ src/components/PostListItem/index.tsx
@@ -1,14 +1,15 @@
-import { Box, Flex, Heading, Icon, Tag, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Icon, Link, Tag, Text } from "@chakra-ui/react";
 import { format } from "date-fns";
 import React from "react";
 import { MdEditCalendar, MdFolder } from "react-icons/md";
+import NextLink from "next/link";
 
 type Props = {
   post: Post;
 };
 
 export const PostListItem = ({ post }: Props) => {
-  const { id, tags, title, publishedAt } = post;
+  const { id, tags, title, publishedAt, category } = post;
   const formatDate = (dateString: string) => {
     return format(new Date(dateString), "yyyy/MM/dd");
   };
@@ -16,20 +17,30 @@ export const PostListItem = ({ post }: Props) => {
   return (
     <Box borderBottom="solid 1px" borderColor="gray.200" p="2" mb="4">
       <Heading as="h2" size="lg" mb="4">
-        {title}
+        <NextLink href={`/posts/${id}`}>
+          <Link _hover={{ textDecor: "none" }}>{title}</Link>
+        </NextLink>
       </Heading>
       <Flex direction="row" justify="space-between" alignItems="center">
         <Flex direction="row" alignItems="flex-end" padding={1}>
           <Icon as={MdEditCalendar} mr={1} />
           <Text fontSize="xs">{formatDate(publishedAt)}</Text>
-          <Icon as={MdFolder} ml={2} mr={1} />
-          <Text fontSize="xs">Category</Text>
+          <NextLink href={`/categories/${category.id}`}>
+            <Link fontSize="xs" _hover={{ textDecor: "none" }}>
+              <Icon as={MdFolder} ml={2} mr={1} />
+              {category.name}
+            </Link>
+          </NextLink>
         </Flex>
         <Flex direction="row" alignItems="flex-end" padding={1}>
           {tags?.map((tag) => (
-            <Tag mr="1" size="sm" key={tag}>
-              {tag}
-            </Tag>
+            <NextLink href={`/tags/${tag.id}`} key={tag.id}>
+              <Link _hover={{ textDecor: "none" }}>
+                <Tag mr="1" size="sm">
+                  {tag.name}
+                </Tag>
+              </Link>
+            </NextLink>
           ))}
         </Flex>
       </Flex>
