28b4bab90bb17b08b7457bad9318bb9ad03b1fa6
mt-coff <mt.coff.00@gmail.com>
2021-12-20 00:55:32 +0900

    gtag

---
+++ src/lib/gtag.ts
@@ -0,0 +1,8 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+export const pageview = (url: string) => {
+  GA_TRACKING_ID &&
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,53 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { theme } from "../config/chakra";
 import { CommonLayout } from "../components/CommonLayout";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider theme={theme}>
-      <CommonLayout>
-        <Component {...pageProps} />
-      </CommonLayout>
-    </ChakraProvider>
+    <>
+      {gtag.GA_TRACKING_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </>
+      )}
+      <ChakraProvider theme={theme}>
+        <CommonLayout>
+          <Component {...pageProps} />
+        </CommonLayout>
+      </ChakraProvider>
+    </>
   );
 }
 
