f63c1ef0cec2ac0ab8a4bb91dfb7734a1e1cfb89
mt-coff <mt.coff.00@gmail.com>
2021-12-31 14:02:18 +0900

    :recycle: og image

--- src/scripts/generate-ogp.mjs
+++ src/scripts/generate-ogp.mjs
@@ -1,78 +1,51 @@
-import { getAllPost } from "./getAllPost.mjs";
 import path from "path";
+import { chromium } from "playwright-chromium";
 import fs from "fs/promises";
-import canvas from "canvas";
+import Mustache from "mustache";
 
-const W = 1200;
-const H = 630;
-const LineHeight = 48;
+const width = 1200;
+const height = 630;
+const blogTitle = "blog.mt_coff";
+const imgSrc = await fs
+  .readFile(path.join(process.cwd(), "/public/image/mt_coff.jpeg"), {
+    encoding: "base64",
+  })
+  .then((buf) => buf);
+const htmlTemplate = await fs
+  .readFile(path.join(process.cwd(), "/src/scripts/template/template.html"))
+  .then((buf) => buf.toString());
 
-export const generateOGP = async () => {
-  const posts = await getAllPost();
-  let noTitleCnt = 0;
-  for (const post of posts) {
-    if (!post.title) {
-      noTitleCnt++;
-      continue;
-    }
-    const img = await generateImg(post.title);
-    const targetPath = path.join(
-      process.cwd(),
-      `./public/image/${post.filename}.png`
-    );
-    fs.writeFile(targetPath, img);
-  }
-
-  if (noTitleCnt) {
-    console.log(`${noTitleCnt} article titles are not set`);
-  }
-};
-
-const generateImg = async (text) => {
-  const c = canvas.createCanvas(W, H);
-  const ctx = c.getContext("2d");
-  const img = new canvas.Image();
-
-  ctx.fillStyle = "#c0ffee";
-  ctx.fillRect(0, 0, W, H);
-  ctx.fillStyle = "white";
-  ctx.fillRect(20, 20, W - 40, H - 40);
-  img.onload = () => {
-    ctx.drawImage(
-      img,
-      W - img.naturalWidth / 5 - 30,
-      H - img.naturalHeight / 5 - 30,
-      img.naturalWidth / 5,
-      img.naturalHeight / 5
-    );
+export const generateOGP = async (posts) => {
+  const browser = await chromium.launch();
+  const ctx = await browser.newContext({
+    viewport: {
+      height,
+      width,
+    },
+    deviceScaleFactor: 1,
+  });
+  const page = await ctx.newPage();
+  const genScreenshot = async (html, filename) => {
+    await page.setContent(html);
+    await page.screenshot({
+      omitBackground: false,
+      path: path.join(process.cwd(), `./public/image/${filename}.png`),
+    });
   };
-  img.src = path.join(process.cwd(), "./public/image/mt_coff.jpeg");
 
-  ctx.font = `normal ${LineHeight}px Impact`;
-  ctx.fillStyle = "black";
-  // ctx.fillText(text, img.naturalWidth / 4 + 100, H / 2);
-  renderMultiLineText(ctx, text, (W / 5) * 3, H / 2);
-  const m = ctx.measureText("blog.mt_coff");
-  ctx.fillText("blog.mt_coff", (W - m.width) / 2, H - 50);
+  for (const post of posts) {
+    const html = genHTMLFromPost(post);
+    await genScreenshot(html, post.filename);
+  }
 
-  return c.toBuffer();
+  browser.close();
 };
 
-const renderMultiLineText = (ctx, text, maxWidth, startHeight) => {
-  let tmp = "";
-  let row = 0;
-  let m;
-  const txtArr = Array.from(text);
-
-  for (const char of txtArr) {
-    m = ctx.measureText(tmp);
-    if (m.width <= maxWidth) {
-      tmp += char;
-    } else {
-      ctx.fillText(tmp, (W - m.width) / 2, startHeight + row * LineHeight);
-      row++;
-      tmp = "";
-    }
-  }
-  ctx.fillText(tmp, (W - m.width) / 2, startHeight + row * LineHeight);
+const genHTMLFromPost = (post) => {
+  const { title } = post;
+  return Mustache.render(htmlTemplate, {
+    title,
+    blogTitle,
+    imgSrc: `data:image/png;base64,${imgSrc}`,
+  });
 };
--- src/scripts/runGenerate.mjs
+++ src/scripts/runGenerate.mjs
@@ -1,10 +1,12 @@
 import { generateJSON } from "./generate-json.mjs";
 import { generateOGP } from "./generate-ogp.mjs";
+import { getAllPost } from "./getAllPost.mjs";
 
-const main = () => {
+const main = async () => {
+  const posts = await getAllPost();
   generateJSON("category", "categories");
   generateJSON("tags");
-  generateOGP();
+  generateOGP(posts);
 };
 
 main();
